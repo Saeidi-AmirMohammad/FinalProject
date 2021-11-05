a0f86bd823ea99aedb8ae310ae15c212623472b6
SaeidiAmirMohammad <amirmohammad1379.saeidi@gmail.com>
2021-11-05 10:36:50 +0330

    set error message

--- app/Controll/User/userController.php
+++ app/Controll/User/userController.php
@@ -26,24 +26,36 @@ if (isPost()) {
         $user = createUser($connect, $_POST);
         if ($user){
             $error=true;
+            $_SESSION['error'] = true;
+            $_SESSION['massage'] = 'باموفقیت ثبت شد';
+            $_SESSION['type'] = 'success';
         }else{
             $error=false;
+            $_SESSION['error'] = true;
+            $_SESSION['massage'] = 'کد ملی ، ایمیل ، شماره شناسنامه و تلفن همراه نمی توانند تکراری باشند';
+            $_SESSION['type'] = 'danger';
         }
     }else{
         $error=false;
+        $_SESSION['error'] = true;
+        $_SESSION['massage'] = 'مقدار صحیحی را وارد کنید';
+        $_SESSION['type'] = 'danger';
     }
 }else{
     $error=false;
-}
-if ($error == true) {
     $_SESSION['error'] = true;
-    $_SESSION['massage'] = 'باموفقیت ثبت شد';
-    $_SESSION['type'] = 'success';
-} else {
-    $_SESSION['error'] = true;
-    $_SESSION['massage'] = 'خطایی رخ داده است لطفا برسی نمایید';
+    $_SESSION['massage'] = 'لطفا درخواست خود را به صورت post ارسال کیند';
     $_SESSION['type'] = 'danger';
 }
+//if ($error == true) {
+//    $_SESSION['error'] = true;
+//    $_SESSION['massage'] = 'باموفقیت ثبت شد';
+//    $_SESSION['type'] = 'success';
+//} else {
+//    $_SESSION['error'] = true;
+//    $_SESSION['massage'] = 'خطایی رخ داده است لطفا برسی نمایید';
+//    $_SESSION['type'] = 'danger';
+//}
 reDirect("../../../View/user/create.php");
 //echo "<pre>";
 //var_dump($_POST);
